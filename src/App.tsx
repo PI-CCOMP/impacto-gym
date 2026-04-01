e7a54fd468e728bb5bf354f313c10931ae6b941c
Arthur <thurxy0@gmail.com>
2026-04-01 17:46:16 -0300

    refactor: Ajusted index route

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import "./styles/theme.css";
 import "./styles/global.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Auth } from "./pages/Auth";
+import { Index } from "./pages/Index";
 import { Componentes } from "./Componentes";
 
 export function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Componentes />} />
-        <Route path="/auth" element={<Auth />} />
+        <Route path="/componentes" element={<Componentes />} />
+        <Route path="/" element={<Index />} />
       </Routes>
     </BrowserRouter>
   );
