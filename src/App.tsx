556de9dd1595967d11947644d80cd38fb71abbf6
Arthur <thurxy0@gmail.com>
2026-04-01 18:43:56 -0300

    feat: added register route

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Componentes } from "./Componentes";
 
 import { Index } from "./pages/Index";
 import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
 
 export function App() {
   return (
@@ -14,6 +15,7 @@ export function App() {
         <Route path="/componentes" element={<Componentes />} />
         <Route path="/" element={<Index />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
