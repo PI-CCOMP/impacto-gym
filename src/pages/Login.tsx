8e56bd52faf51aad1d6e6064c30a49c7d84862f0
Arthur <thurxy0@gmail.com>
2026-04-01 18:19:27 -0300

    feat: Added login page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,38 @@
+import { Wrapper } from "../components/Wrapper";
+import { Container } from "../components/Container";
+import { PageHeader } from "../components/PageHeader";
+import { Logo } from "../components/Logo";
+import { AuthFormHeader } from "../components/AuthFormHeader";
+import { Input } from "../components/Input/input";
+import { Button } from "../components/Button";
+
+import styles from "../pages/Login.module.css";
+
+export function Login() {
+  return (
+    <Wrapper>
+      <Container>
+        <PageHeader>Login</PageHeader>
+        <Logo />
+        <AuthFormHeader>Conecte-se!</AuthFormHeader>
+        <form action="/home" method="post">
+          <Input
+            type="text"
+            labelText="CPF ou E-mail"
+            id="identifier"
+            name="identifier"
+            placeholder="123.456.789-10"
+          />
+          <Input
+            type="password"
+            labelText="Senha"
+            id="password"
+            name="password"
+            placeholder="*********"
+          />
+          <Button>Enviar</Button>
+        </form>
+      </Container>
+    </Wrapper>
+  );
+}
