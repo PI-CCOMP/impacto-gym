050beb222bc9561139681dd1843e33ca7d5c3ff3
Arthur <thurxy0@gmail.com>
2026-04-29 14:17:08 -0300

    Adjustment to the validation of the 'go back one page' function

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,17 +1,25 @@
-import styles from "./styles.module.css";
-
 import { useNavigate } from "react-router-dom";
+import styles from "./styles.module.css";
 import { ArrowLeft } from "lucide-react";
 
 type PageHeaderProps = {
-  children: React.ReactNode;
+  onBack?: () => void;
+  children: string;
 };
 
-export function PageHeader({ children }: PageHeaderProps) {
+export function PageHeader({ onBack, children }: PageHeaderProps) {
   const navigate = useNavigate();
 
+  function handleBack() {
+    if (onBack) {
+      onBack();
+    } else {
+      navigate(-1);
+    }
+  }
+
   return (
-    <button onClick={() => navigate(-1)} className={styles.previousPage}>
+    <button onClick={handleBack} className={styles.previousPage}>
       <ArrowLeft className={styles.icon} />
       <h1 className={styles.title}>{children}</h1>
     </button>
