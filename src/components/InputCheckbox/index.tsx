0df8cab5006e15e7b41f55aea45faa618e204385
Arthur <thurxy0@gmail.com>
2026-04-29 19:39:31 -0300

    allow validations at checkbox

--- src/components/InputCheckbox/index.tsx
+++ src/components/InputCheckbox/index.tsx
@@ -1,20 +1,35 @@
 import styles from "./styles.module.css";
 
 type InputCheckboxProps = {
-  labelText?: string;
-} & React.ComponentProps<"input">;
+  labelText: string;
+  checked?: boolean;
+  disabled?: boolean;
+  onChange?: (id: string, checked: boolean) => void;
+} & Omit<React.ComponentProps<"input">, "onChange">;
 
-export function InputCheckbox({ labelText, id, ...props }: InputCheckboxProps) {
-  if (labelText) {
-    return (
-      <div className={styles.container}>
-        <input type="checkbox" id={id} className={styles.input} {...props} />
-        <label htmlFor={id} className={styles.label}>
-          {labelText}
-        </label>
-      </div>
-    );
-  }
-
-  return <input type="checkbox" id={id} className={styles.input} {...props} />;
+export function InputCheckbox({
+  labelText,
+  id,
+  checked,
+  disabled,
+  onChange,
+}: InputCheckboxProps) {
+  return (
+    <div className={styles.container}>
+      <input
+        type="checkbox"
+        id={id}
+        checked={checked}
+        disabled={disabled}
+        onChange={(e) => onChange?.(id as string, e.target.checked)}
+        className={styles.input}
+      />
+      <label
+        htmlFor={id}
+        className={`${styles.label} ${disabled ? styles.disabled : ""}`}
+      >
+        <span>{labelText}</span>
+      </label>
+    </div>
+  );
 }
