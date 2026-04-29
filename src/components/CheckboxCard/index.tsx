f00918149badd41b1789b3ed0048bc1c743dc020
Arthur <thurxy0@gmail.com>
2026-04-29 19:34:02 -0300

    fix: add dd validations to allow submission

--- src/components/CheckboxCard/index.tsx
+++ src/components/CheckboxCard/index.tsx
@@ -4,15 +4,40 @@ type CheckboxCardProps = {
   image: string;
   labelText: string;
   alt: string;
-} & React.ComponentProps<"input">;
+  name: string;
+  checked?: boolean;
+  onChange?: (value: string) => void;
+} & Omit<React.ComponentProps<"input">, "onChange">;
 
-export function CheckboxCard({ image, alt, labelText, id }: CheckboxCardProps) {
+export function CheckboxCard({
+  image,
+  alt,
+  labelText,
+  id,
+  name,
+  value,
+  checked,
+  onChange,
+}: CheckboxCardProps) {
+  function handleClick() {
+    onChange?.(value as string);
+  }
+
   return (
     <div className={styles.container}>
       <label htmlFor={id} className={styles.label}>
         <img src={image} alt={alt} className={styles.img} />
         <span>{labelText}</span>
-        <input type="checkbox" id={id} className={styles.input} hidden />
+        <input
+          type="radio"
+          id={id}
+          name={name}
+          value={value as string}
+          checked={checked}
+          onChange={handleClick}
+          className={styles.input}
+          hidden
+        />
       </label>
     </div>
   );
