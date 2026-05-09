05fadcbcf05d907b05f6d6fb69bd8e500bf84b87
Arthur <thurxy0@gmail.com>
2026-05-09 16:10:22 -0300

    feat: create UserCard component

---
+++ src/components/UserCard/index.tsx
@@ -0,0 +1,31 @@
+import styles from "./styles.module.css";
+
+type UserCardProps = {
+  id: string;
+  image: string;
+  name: string;
+  cpf: string;
+  instructor?: string;
+  onClick?: () => void;
+};
+
+export function UserCard({ id, image, name, cpf, instructor }: UserCardProps) {
+  return (
+    <div className={styles.container}>
+      <img src={image} className={styles.img} />
+      <div className={styles.content}>
+        <p>{name}</p>
+        <p>
+          <b>CPF: </b>
+          {cpf}
+        </p>
+        {instructor && (
+          <p>
+            <b>Instrutor: </b>
+            {instructor}
+          </p>
+        )}
+      </div>
+    </div>
+  );
+}
