3bb43a83c5d61b9613613bd2497008bbf4981fd9
Arthur <thurxy0@gmail.com>
2026-05-09 11:12:53 -0300

    fix: width component

--- src/pages/Dashboard/DashboardUsers.tsx
+++ src/pages/Dashboard/DashboardUsers.tsx
@@ -5,10 +5,6 @@ export function DashboardUsers() {
   return (
     <Container>
       <SideMenu />
-      <div>
-        <h1>Usuários</h1>
-        teste teste
-      </div>
     </Container>
   );
 }
