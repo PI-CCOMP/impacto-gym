15e8895896145c4bc2883aa989ddb07b4df9f071
Arthur <thurxy0@gmail.com>
2026-05-09 16:11:24 -0300

    refactor: add bgcolor red

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,11 +2,20 @@ import styles from "./styles.module.css";
 
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   children: React.ReactNode;
+  variant?: "blue" | "red";
 };
 
-export function Button({ children, ...props }: ButtonProps) {
+export function Button({
+  children,
+  variant = "blue",
+  className,
+  ...props
+}: ButtonProps) {
   return (
-    <button {...props} className={styles.button}>
+    <button
+      {...props}
+      className={`${styles.button} ${styles[variant]} ${className || ""}`}
+    >
       {children}
     </button>
   );
